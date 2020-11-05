30348286d3ac5815111fa04c05fead55dab3bd6e
Elvar Laxdal <elvarlax@gmail.com>
2020-11-05 15:14:16 +0100

    Frequency function

--- cloudsafe.py
+++ cloudsafe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import geopandas as gpd
 import sqlite3
 from sqlite3 import Error
 
@@ -20,15 +21,21 @@ def select_all_stations(conn):
     return pd.read_sql("SELECT * FROM stations", conn)
 
 
+def select_all_bus_stops():
+    return gpd.read_file('here-api/bus-stops.geojson')
+
+
+def frequency(df):
+    return df.value_counts()
+
+
 # Only for testing (Use the Jupyter Notebook for Visualization)
 if __name__ == "__main__":
     database = 'here-api/main.db'
     conn = create_connection(database)
-
     departures = select_all_departures(conn)
     stations = select_all_stations(conn)
-
-    print(departures)
-    print(stations)
-
+    bus_stops = select_all_bus_stops()
+    freq_dep = frequency(departures)
+    freq_sta = frequency(stations)
     conn.close()
