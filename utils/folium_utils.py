8b3954645a52cf42188752cbbfd89ce1386f22b0
Matas <mk@cerebriu.com>
2020-11-08 20:43:10 +0100

    Heatmap grid

---
+++ utils/folium_utils.py
@@ -0,0 +1,68 @@
+import numpy 
+# https://www.jpytr.com/post/analysinggeographicdatawithfolium/
+def get_geojson_grid(upper_right, lower_left, n=6):
+    """Returns a grid of geojson rectangles, and computes the exposure in each section of the grid based on the vessel data.
+
+    Parameters
+    ----------
+    upper_right: array_like
+        The upper right hand corner of "grid of grids" (the default is the upper right hand [lat, lon] of the USA).
+
+    lower_left: array_like
+        The lower left hand corner of "grid of grids"  (the default is the lower left hand [lat, lon] of the USA).
+
+    n: integer
+        The number of rows/columns in the (n,n) grid.
+
+    Returns
+    -------
+
+    list
+        List of "geojson style" dictionary objects   
+    """
+
+    all_boxes = []
+
+    lat_steps = numpy.linspace(lower_left[0], upper_right[0], n+1)
+    lon_steps = numpy.linspace(lower_left[1], upper_right[1], n+1)
+
+    lat_stride = lat_steps[1] - lat_steps[0]
+    lon_stride = lon_steps[1] - lon_steps[0]
+
+    for lat in lat_steps[:-1]:
+        for lon in lon_steps[:-1]:
+            # Define dimensions of box in grid
+            upper_left = [lon, lat + lat_stride]
+            upper_right = [lon + lon_stride, lat + lat_stride]
+            lower_right = [lon + lon_stride, lat]
+            lower_left = [lon, lat]
+
+            # Define json coordinates for polygon
+            coordinates = [
+                upper_left,
+                upper_right,
+                lower_right,
+                lower_left,
+                upper_left
+            ]
+
+            geo_json = {"type": "FeatureCollection",
+                        "properties":{
+                            "lower_left": lower_left,
+                            "upper_right": upper_right
+                        },
+                        "features":[]}
+
+            grid_feature = {
+                "type":"Feature",
+                "geometry":{
+                    "type":"Polygon",
+                    "coordinates": [coordinates],
+                }
+            }
+
+            geo_json["features"].append(grid_feature)
+
+            all_boxes.append(geo_json)
+
+    return all_boxes
