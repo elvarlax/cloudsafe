3a89d7ec4026e03660324ffab75312a17db20dd7
Elvar Laxdal <elvarlax@gmail.com>
2020-11-11 14:33:37 +0100

    Fixing heatmap

--- cloudsafe.py
+++ cloudsafe.py
@@ -1,7 +1,13 @@
-import pandas as pd
-import geopandas as gpd
 import sqlite3
 from sqlite3 import Error
+
+import folium
+import geopandas as gpd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from utils.folium_utils import get_geojson_grid
 
 
 def create_connection(db_file):
@@ -29,6 +35,13 @@ def frequency(df):
     return df.value_counts()
 
 
+def get_coords(row):
+    if row['coordinates_here']:
+        coords = row['coordinates_here'].split(',')
+        return coords[0], coords[1]
+    return None, None
+
+
 # Only for testing (Use the Jupyter Notebook for Visualization)
 if __name__ == "__main__":
     database = 'here-api/main.db'
@@ -36,6 +49,58 @@ if __name__ == "__main__":
     departures = select_all_departures(conn)
     stations = select_all_stations(conn)
     bus_stops = select_all_bus_stops()
-    freq_dep = frequency(departures)
-    freq_sta = frequency(stations)
+
+    coords = stations.iloc[0].coordinates_here.split(',')
+    m = folium.Map([coords[0], coords[1]], zoom_start=10)
+    stations[['latitude', 'longitude']] = stations.apply(get_coords, axis=1, result_type="expand")
+
+    # convert to (n, 2) nd-array format for heatmap
+    stationArr = stations[['latitude', 'longitude']].to_numpy()
+
+    # choropleth map
+    frequencies = stations.name_overpass.value_counts()
+
+    # stations
+    rows = []
+    for station in frequencies.keys():
+        first = stations.loc[stations['name_overpass'] == station].iloc[0]
+        rows.append({'station_name': station,
+                     'longitude': first.longitude,
+                     'latitude': first.latitude,
+                     'bus_count': frequencies.get(station)})
+
+    final = pd.DataFrame(rows)
+    final['longitude'] = final['longitude'].astype(float)
+    final['latitude'] = final['latitude'].astype(float)
+
+    top_right = [final['latitude'].max(), final['longitude'].max()]
+    bottom_left = [final['latitude'].min(), final['longitude'].min()]
+    grid = get_geojson_grid(top_right, bottom_left)
+
+    counts = []
+
+    for box in grid:
+        upper_right = box["properties"]["upper_right"]
+        lower_left = box["properties"]["lower_left"]
+        items = final.loc[(final['latitude'] <= upper_right[1]) &
+                          (final['longitude'] <= upper_right[0]) &
+                          (final['longitude'] >= lower_left[0]) &
+                          (final['latitude'] >= lower_left[1])]
+        counts.append(len(items))
+    max_count = max(counts)
+
+    for i, geo_json in enumerate(grid):
+        color = plt.cm.Reds(counts[i] / max_count)
+        color = mpl.colors.to_hex(color)
+        gj = folium.GeoJson(geo_json,
+                            style_function=lambda feature, color=color: {
+                                'fillColor': color,
+                                'color': "black",
+                                'weight': 2,
+                                'dashArray': '5, 5',
+                                'fillOpacity': 0.55,
+                            })
+
+        m.add_child(gj)
+
     conn.close()
