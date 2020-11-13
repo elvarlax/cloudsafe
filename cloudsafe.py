478b30f8ce84a4517a45927870943e42b2f1f18d
Elvar Laxdal <elvarlax@gmail.com>
2020-11-13 15:15:05 +0100

    Filter by hour done

--- cloudsafe.py
+++ cloudsafe.py
@@ -1,6 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-
 import pandas as pd
 
 
@@ -13,10 +12,14 @@ def create_connection(db_file):
     return conn
 
 
+def text_to_datetime(text):
+    return pd.to_datetime(text, format='%H:%M')
+
+
 def filter_by_hour(df, start, end):
-    start = pd.to_datetime(start, format='%H:%M')
-    end = pd.to_datetime(end, format='%H:%M')
-    return df[start >= df['time'] < end]
+    start = text_to_datetime(start)
+    end = text_to_datetime(end)
+    return df[(df['time'] >= start) & (df['time'] < end)]
 
 
 def select_all_departures(conn):
@@ -29,7 +32,7 @@ def select_all_stations(conn):
 
 def select_stations_join_departures(conn):
     df = pd.read_sql("SELECT * FROM stations INNER JOIN departures d on stations.id = d.station_id", conn)
-    df['time'] = pd.to_datetime(df['time'], format='%H:%M')
+    df['time'] = text_to_datetime(df['time'])
     return df
 
 
@@ -45,5 +48,5 @@ if __name__ == "__main__":
     database = 'here-api/main.db'
     conn = create_connection(database)
     stations = select_stations_join_departures(conn)
-    stations_between_10_and_11 = filter_by_hour(stations, "10:00", "11:00")
-    print(stations_between_10_and_11)
+    stations_filtered = filter_by_hour(stations, "08:00", "09:00")
+    print(stations_filtered)
