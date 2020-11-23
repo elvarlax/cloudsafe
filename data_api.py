35e2158dc0986b38c789761d61f88bd1fc8f6c0f
Paul Cseidi <s182859@student.dtu.dk>
2020-11-23 20:21:00 +0100

    Added timestamped heatmap (extra QA needed)

--- data_api.py
+++ data_api.py
@@ -9,8 +9,8 @@ import pandas as pd
 import folium
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
-
+from datetimerange import DateTimeRange
+import datetime
 from pathlib import Path
 
 db = Path.cwd() / 'data/main.db'
@@ -64,13 +64,13 @@ def get_routes_geojson(bus_ids, flip_coordinates=True):
         WHERE id IN ({','.join(['?'] * len(bus_ids))});"""
     buses = cursor.execute(stmt, bus_ids).fetchall()
 
-    for bus in buses:
+    for bus_id, name, from_station, to_station in buses:
         stmt = 'SELECT DISTINCT segment FROM routes_points WHERE bus_id = ? ORDER BY segment;'
-        segments = cursor.execute(stmt, [bus[0]])
+        segments = cursor.execute(stmt, [bus_id]).fetchall()
         coordinates_list = []
         for segment in segments:
             stmt_sub = 'SELECT coordinates FROM routes_points WHERE bus_id = ? AND segment = ? ORDER BY segment, seq;'
-            coordinates = cursor.execute(stmt_sub, (bus[0], segment[0])).fetchall()
+            coordinates = cursor.execute(stmt_sub, (bus_id, segment[0])).fetchall()
             coordinates_list.append(
                 [list(map(float, row[0].split(',')[::-1] if flip_coordinates else row[0].split(','))) for row in coordinates]
             )
@@ -78,12 +78,10 @@ def get_routes_geojson(bus_ids, flip_coordinates=True):
         route = {
             "type": "Feature",
             "properties": {
-                "bus_id": bus[0],
-                "name": bus[1],
-                "from": bus[2],
-                "to": bus[3],
-                # "stroke": "#ef500a",
-                # "stroke-width": 3
+                "bus_id": bus_id,
+                "name": name,
+                "from": from_station,
+                "to": to_station
             },
             "geometry": {
                 "type": "MultiLineString",
@@ -102,12 +100,15 @@ def get_grid_geojson(bus_ids, time_range, flip_coordinates=True, stations_source
     """
     Generate the grid GeoJson based on the values saved in 'grid_cells' table.
     This can be used to overlay it on top of the routes in the jupyter notebooks.
+    For each hour in the time interval it will generate a grid.
     It also maps the bus stations to each cell in order to provide 'buses_count_subset' and
-    'buses_count_total' in the returned GeoJson.
+    'buses_count_total' in the returned GeoJson, that are the frequency of buses (counted at a single station)
+    for the given bus_ids in each cell and the frequency of all buses in each cell. This can be used as a ratio
+    to show the percentage of the subset in terms of max frequencies.
 
     Args:
         bus_ids (list): List of the bus ids.
-        time_range (tuple): Time range of format ('saturday', '10:03', '11:03'). 
+        time_range (tuple): Time range in format ('saturday', '10:03', '11:03').
         flip_coordinates (bool, optional): Flip the coordinates. Defaults to True.
         stations_source (str, optional): The source of the stations' coordinates. 
         Valid values: 'overpass', 'here'. Defaults to 'overpass'.
@@ -118,6 +119,40 @@ def get_grid_geojson(bus_ids, time_range, flip_coordinates=True, stations_source
 
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
+    
+    if stations_source == 'overpass':
+        stmt_buses_subset = f"""SELECT cell_id, substr(time, 0, 3) AS interval, count(*) FROM departures d, stations_cells_overpass sc 
+            WHERE d.station_id = sc.station_id AND bus_id IN ({','.join(['?'] * len(bus_ids))}) AND day = ? AND time BETWEEN ? AND ? 
+            GROUP BY cell_id, interval;"""
+        
+        stmt_buses_all = f"""SELECT cell_id, substr(time, 0, 3) as interval, count(*) FROM departures d, stations_cells_overpass sc 
+            WHERE d.station_id = sc.station_id AND day = ? AND time BETWEEN ? AND ? GROUP BY cell_id, interval;"""
+    
+    elif stations_source == 'here':
+        stmt_buses_subset = f"""SELECT cell_id, substr(time, 0, 3) as interval, count(*) FROM departures d, stations_cells_here sc 
+            WHERE d.station_id = sc.station_id AND bus_id IN ({','.join(['?'] * len(bus_ids))}) AND day = ? AND time BETWEEN ? AND ? 
+            GROUP BY cell_id, interval;"""
+        
+        stmt_buses_all = f"""SELECT cell_id, substr(time, 0, 3) as interval, count(*) FROM departures d, stations_cells_here sc 
+            WHERE d.station_id = sc.station_id AND day = ? AND time BETWEEN ? AND ? GROUP BY cell_id, interval;"""
+   
+    else:
+        cursor.close()
+        conn.close()
+        raise Exception('Invalid stations_source!')
+
+
+    buses_count_subset = {}
+    for cell, interval, count in cursor.execute(stmt_buses_subset, (*bus_ids, *time_range)).fetchall():
+        d = {}
+        d[interval] = count
+        buses_count_subset.setdefault(cell, {}).update(d)
+    
+    buses_count_total = {}
+    for cell, interval, count in cursor.execute(stmt_buses_all, [*time_range]).fetchall():
+        d = {}
+        d[interval] = count
+        buses_count_total.setdefault(cell, {}).update(d)
 
     geo_json = {
         "type": "FeatureCollection",
@@ -127,56 +162,59 @@ def get_grid_geojson(bus_ids, time_range, flip_coordinates=True, stations_source
         },
         "features": []
     }
-    
-    if stations_source == 'overpass':
-        stmt_buses_subset = f"""SELECT cell_id, count(*) FROM departures d, stations_cells_overpass sc WHERE d.station_id = sc.station_id 
-            AND bus_id IN ({','.join(['?'] * len(bus_ids))}) AND day = ? AND time BETWEEN ? AND ? GROUP BY cell_id;"""
-        stmt_buses_all = f"""SELECT cell_id, count(*) FROM departures d, stations_cells_overpass sc WHERE d.station_id = sc.station_id
-            AND day = ? GROUP BY cell_id;""" # AND time BETWEEN ? AND ?
-    
-    elif stations_source == 'here':
-        stmt_buses_subset = f"""SELECT cell_id, count(*) FROM departures d, stations_cells_here sc WHERE d.station_id = sc.station_id 
-            AND bus_id IN ({','.join(['?'] * len(bus_ids))}) AND day = ? AND time BETWEEN ? AND ? GROUP BY cell_id;"""
-        stmt_buses_all = f"""SELECT cell_id, count(*) FROM departures d, stations_cells_here sc WHERE d.station_id = sc.station_id
-            AND day = ? GROUP BY cell_id;""" # AND time BETWEEN ? AND ?
-   
-    else:
-        raise Exception('Invalid stations_source!')
 
-    buses_count_subset = dict(cursor.execute(stmt_buses_subset, (*bus_ids, *time_range)).fetchall())
-
-    buses_count_total = dict(cursor.execute(stmt_buses_all, [time_range[0]]).fetchall())
+    tr = DateTimeRange(*time_range[-2:])
 
     stmt = 'SELECT id, x_axis, y_axis, upper_left, upper_right, lower_right, lower_left FROM grid_cells;'
 
-    for cell in cursor.execute(stmt).fetchall():
+    for cell_id, x_axis, y_axis, u_left, u_right, l_right, l_left in cursor.execute(stmt).fetchall():
         coordinates_list = [
-            list(map(float, cell[3].split(',')[::-1] if flip_coordinates else cell[3].split(','))),
-            list(map(float, cell[4].split(',')[::-1] if flip_coordinates else cell[4].split(','))),
-            list(map(float, cell[5].split(',')[::-1] if flip_coordinates else cell[5].split(','))),
-            list(map(float, cell[6].split(',')[::-1] if flip_coordinates else cell[6].split(',')))
+            list(map(float, u_left.split(',')[::-1] if flip_coordinates else u_left.split(','))),
+            list(map(float, u_right.split(',')[::-1] if flip_coordinates else u_right.split(','))),
+            list(map(float, l_right.split(',')[::-1] if flip_coordinates else l_right.split(','))),
+            list(map(float, l_left.split(',')[::-1] if flip_coordinates else l_left.split(',')))
         ]
 
-        subset = buses_count_subset[cell[0]] if cell[0] in buses_count_subset else 0
-        total = buses_count_total[cell[0]] if cell[0] in buses_count_total else 0
+        for time in tr.range(datetime.timedelta(hours=1)):
+            time_hours = time.strftime('%H')
+            if cell_id in buses_count_subset and time_hours in buses_count_subset[cell_id]:
+                subset = buses_count_subset[cell_id][time_hours]
+            else:
+                subset = 0
 
-        cell_color = plt.cm.get_cmap('Reds')(subset / total if total else 0)
-        cell_color = mpl.colors.to_hex(cell_color)
+            if cell_id in buses_count_total and time_hours in buses_count_total[cell_id]:
+                total = buses_count_total[cell_id][time_hours]
+            else:
+                total = 0
 
-        cell = {
-            "type": "Feature",
-            "properties": {
-                "matrix_coordinates": f"({cell[1]},{cell[2]})",
-                "buses_count_subset": subset,
-                "buses_count_total": total,
-                "fillColor": cell_color, #"#67000d"
-            },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [coordinates_list]
+            perc = subset/total if total else 1
+            cell_color = plt.cm.get_cmap('Reds')(perc * 5) # 5 - to increase the shade
+            cell_color = mpl.colors.to_hex(cell_color)
+            perc = round(perc * 100, 2)
+
+            cell = {
+                "type": "Feature",
+                "properties": {
+                    "matrix_coordinates": f"({x_axis},{y_axis})",
+                    "buses_count_subset": subset,
+                    "buses_count_total": total,
+                    "popup": f'({x_axis}, {y_axis}) {subset}/{total} ({perc}%)',
+                    # "fillColor": cell_color,
+                    "style": {
+                        'fillColor': cell_color,
+                        'color': 'black',
+                        'weight': 0.5,
+                        'dashArray': '5',
+                        'fillOpacity': 0.5
+                    },
+                    "time": "2020-10-10T" + time.strftime('%H:%M') + ":00"
+                },
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [coordinates_list]
+                }
             }
-        }
-        geo_json['features'].append(cell)
+            geo_json['features'].append(cell)
 
     cursor.close()
     conn.close()
@@ -205,10 +243,10 @@ def get_route_cells(bus_ids):
     route_cells = cursor.execute(stmt, bus_ids).fetchall()
 
     route_cells_agg = {}
-    for route_cell in route_cells:
-        if route_cell[0] not in route_cells_agg.keys():
-            route_cells_agg[route_cell[0]] = []
-        route_cells_agg[route_cell[0]].append((route_cell[1], route_cell[2]))
+    for bus_id, x_axis, y_axis in route_cells:
+        if bus_id not in route_cells_agg.keys():
+            route_cells_agg[bus_id] = []
+        route_cells_agg[bus_id].append((x_axis, y_axis))
 
     cursor.close()
     conn.close()
@@ -249,9 +287,11 @@ def select_stations_join_departures():
 
 
 if __name__ == '__main__':
-    bus_ids = ['23']
-    # bus_ids = [51]
-    print(json.dumps(get_grid_geojson(bus_ids, ('weekday', '05:00', '10:00'))))
+    # bus_ids = [13, 6]
+    # get_grid_geojson(bus_ids, ('weekday', '01:00', '10:00'))
+    data = get_routes_geojson([10])
+    with open('anotherone.json', 'w') as f:
+        json.dump(data, f)
     # print(json.dumps(get_routes_geojson(bus_ids)))
     # bus_ids = [51, 50, 31, 32, 4, 38, 30, 26, 13, 49, 40, 6, 29, 20, 10, 41, 3, 58, 7]
     # stations = select_stations_join_departures()
